771347c9ec05048571369fbbb1ecd35a070defb9
Ikuo Terado <eqobar@gmail.com>
2013-10-01 23:55:14 +0900

    Add cookbook for monit

---
+++ site-cookbooks/monit/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: monit
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "monit" do
+	action :install
+end
